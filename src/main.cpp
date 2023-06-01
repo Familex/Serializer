421dfcfb3c0d2945f82a1e630430e2cd839efe46
Familex <trener2990@gmail.com>
2023-06-01 21:24:00 +0300

    link yaml-cpp via DownloadProject

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
 #include "serializer.h"
+#include "yaml-cpp/yaml.h"
 
-int main() { }
+#include <cassert>
+#include <iostream>
+
+int main()
+{
+    // example from yaml-cpp wiki
+
+    YAML::Node node = YAML::Load("{name: Brewers, city: Milwaukee}");
+    if (node["name"]) {
+        std::cout << node["name"].as<std::string>() << "\n";
+    }
+    if (node["mascot"]) {
+        std::cout << node["mascot"].as<std::string>() << "\n";
+    }
+    assert(node.size() == 2);    // the previous call didn't create a node
+}
